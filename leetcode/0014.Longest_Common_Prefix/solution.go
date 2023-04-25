a653bd27d9c62cc35f84ca1f8f3e46cc4d1496c0
Nam Nguyen <nam.n.nguyen247@gmail.com>
2023-04-25 00:38:00 -0500

    Longest Common Prefix

--- leetcode/0014.Longest_Common_Prefix/solution.go
+++ leetcode/0014.Longest_Common_Prefix/solution.go
@@ -1,10 +1,35 @@
 package _014_Longest_Common_Prefix
 
-//func findCommon(str1 , str2 string) {
-//
-//}
-//
 
-//func longestCommonPrefix(strs []string) string {
-//
-//}
+func longestCommonPrefix(strs []string) string {
+
+	if len(strs) == 0 {
+		return ""
+	}
+
+	if len(strs) == 1 {
+		return strs[0]
+	}
+
+	prefix := strs[0]
+
+	for i := 1; i < len(strs); i++ {
+		mIdx := mismatchIndex(prefix, strs[i])
+		if mIdx == 0 {
+			return ""
+		}
+		prefix = prefix[:mIdx]
+	}
+
+	return prefix
+}
+
+func mismatchIndex(s1 , s2 string) int {
+	i := 0
+	for ; i < len(s1) && i < len(s2); i++ {
+		if s1[i] != s2[i] {
+			return i
+		}
+	}
+	return i
+}
